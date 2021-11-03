ed0dbe1ee98ccfabc5e1909c3cc4fdddd36707bc
ifracaro <74355127+ifracaro@users.noreply.github.com>
2021-11-03 12:18:31 +0100

    Update usecase3.py

--- usecase3.py
+++ usecase3.py
@@ -57,17 +57,18 @@ df8 = pd.DataFrame(np.array([['Numero fattura', '502001905884'], ['Nome Cognome'
 if example == 'Email A':
 
     st.write('\n\n\n')
-    #st.write("DATA RICEZIONE EMAIL: '2021-02-06'")
-    #st.write('Buongiorno,  vi rimando il documento compilato che mi avete mandato su mia richiesta allo scopo di richiedere il cambio di intestazione per fatturazione TARI a Bruna Rossi (mia mamma). Mio papà è deceduto in dicembre quindi mia mamma risulta intestataria delle bollette e unica domiciliata in casa. \n\nPer qualsiasi richiesta ulteriore vi chiedo di scrivere a questa mail. \n\nCordiali saluti, \n\nStefania Contesini')
-    st.image('A.PNG')
-
     but1, but2, but3, but_4, but_5 = st.columns(5)
-    if (but3.button("RUN")):
+
+    if not (but3.button("RUN")):
+        st.image('A1.PNG')
+
+    else:
         gif_runner = st.image('https://media.giphy.com/media/Qw4X3Fsf0N1VqFFUiBi/giphy.gif')
         time.sleep(3)
         gif_runner.empty()
+
+        st.image('A.PNG')
         st.write('\n\n\n')
-        
         st.markdown('<div style="text-align:left"><p class="big-font">Results</p></div>', unsafe_allow_html=True)
 
         kpi1_col, kpi2_col = st.columns(2)
@@ -76,16 +77,13 @@ if example == 'Email A':
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font">  Scoring table </div>', unsafe_allow_html=True)
         kpi1_col.dataframe(df1)
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font"> <b> Informazioni personali estratte </b></p></div>', unsafe_allow_html=True)
-        kpi1_col.dataframe(df2)
-        
+        kpi1_col.dataframe(df2)  
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font"> <b> Presenza ricicli: </b></p></div>', unsafe_allow_html=True)
         kpi1_col.markdown('<div style="text-align:left"><p class="med"> <b> NO </b></p></div>', unsafe_allow_html=True)
 
-       # kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
-      #  kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
+
         original1 = Image.open('MicrosoftTeams-image.png')
         kpi2_col.image(original1, width=40)
-       # kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="medium-font"> <b> Inviata a: </b></p></div>', unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="med"> <b> Voltura team </b></p></div>', unsafe_allow_html=True)
         
@@ -97,19 +95,20 @@ if example == 'Email A':
 if example == 'Email B':
 
     st.write('\n\n\n')
-    #st.write("DATA RICEZIONE EMAIL: '2021-02-26'")
-    #st.write('Buongiorno,\n\nho effettuato un bonifico tramite home banking della fattura  n°0000002160085673 in data 05-02-2021.\nAd oggi sul portale di Iren questa bolletta risulta ancora da pagare.\nPotete controllare?\n\n\n\n\nGrazie.\n\nCordiali saluti,\n\nChiara Belicchi')
-    st.image('B.PNG')
+    but1, but2, but3, but4, but5 = st.columns(5)
 
-    but1, but2, but3, but4, but5 = st.columns(5 )
-    if (but3.button("RUN")):
+    if not (but3.button("RUN")):
+            st.image('B1.PNG')
+
+    else:
         gif_runner2 = st.image('https://media.giphy.com/media/l46Cq6Bro9CsP149q/giphy.gif')
         time.sleep(3)
         gif_runner2.empty()
-        st.write('\n\n\n')
-        
 
+        st.image('B.PNG')
+        st.write('\n\n\n')
         st.markdown('<div style="text-align:left"><p class="big-font">Results</p></div>', unsafe_allow_html=True)
+
         kpi1_col, kpi2_col = st.columns(2)
 
         kpi1_col.markdown('<div style="text-align:right"><p class="med"> <b> Proved Payment </b></p></div>', unsafe_allow_html=True)
@@ -117,14 +116,12 @@ if example == 'Email B':
         kpi1_col.dataframe(df3)
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font"> <b> Informazioni personali estratte </b></p></div>', unsafe_allow_html=True)
         kpi1_col.dataframe(df4)
-
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font"> <b> Presenza ricicli: </b></p></div>', unsafe_allow_html=True)
         kpi1_col.markdown('<div style="text-align:left"><p class="med"> <b> NO </b></p></div>', unsafe_allow_html=True)
 
         
         original1 = Image.open('MicrosoftTeams-image.png')
         kpi2_col.image(original1, width=40)
-       # kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="medium-font"> <b> Inviata a: </b></p></div>', unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="med"> <b> Proved Payment team </b></p></div>', unsafe_allow_html=True)
 
@@ -135,18 +132,18 @@ if example == 'Email B':
 if example == 'Email C':
     
     st.write('\n\n\n')
-    #st.write("DATA RICEZIONE EMAIL: '2021-02-25'")
-    #st.write("Ad oggi, dopo l'invio di tre mail, non ho avuto alcun riscontro sull'avanzamento della pratica che faticosamente mi è stata identificata dal call center come 03299532.\nReitero la richiesta di essere contattato al più presto per comprendere come procedere e in che tempi.\n\nSaluti, \n\nCarlo Bima\n\n---------- Messaggio originale ---------- \n\nDa: carlo.bima@libero.it\n\nA: 'servizioclienti@gruppoiren.it' <servizioclienti@gruppoiren.it>\n\nData: 13/01/2021 13:03\n\nOggetto: Fwd: Franca Gallina - contratto Via degli Albanesi 11/17 16148 Genova\n\n\n\nInvio alla vostra attenzione per la terza volta la documentazione. Dal 5 gennaio che ho ricevuto mail di presa in carico dalla casella postale clienti, non ho avuto alcun riscontro e solo risposte molto lacunose dal vostro call center.\n\nTrovo la situazione scandalosa.\n\nAl momento devo far rientrare mia madre di 92 anni dall'ospedale e sono in enorme difficoltà a causa di IREN per l'assoluta incertezza sui tempi di attivazione della fornitura.\n\nVi chiedo di procedere con la pratica e di darmi un pronto riscontro appena possibile.\n\nCordiali saluti,\n\nCarlo Bima")
-    st.image('C.PNG')
-
     but1, but2, but3, but4, but5 = st.columns(5)
-    if (but3.button("RUN")):
+
+    if not (but3.button("RUN")):
+        st.image('C1.PNG')
+
+    else:
         gif_runner = st.image('https://media.giphy.com/media/Qw4X3Fsf0N1VqFFUiBi/giphy.gif')
         time.sleep(3)
         gif_runner.empty()
+
+        st.image('C.PNG')
         st.write('\n\n\n')
- 
-        
         st.markdown('<div style="text-align:left"><p class="big-font">Results</p></div>', unsafe_allow_html=True)
         kpi1_col, kpi2_col = st.columns(2)
 
@@ -155,14 +152,12 @@ if example == 'Email C':
         kpi1_col.dataframe(df5)
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font"> <b> Informazioni personali estratte </b></p></div>', unsafe_allow_html=True)
         kpi1_col.dataframe(df6)
-
         kpi1_col.markdown('<div style="text-align:right"><p class="medium-font"> <b> Presenza ricicli: </b></p></div>', unsafe_allow_html=True)
         kpi1_col.markdown('<div style="text-align:right"><p class="med"> <b> SI </b></p></div>', unsafe_allow_html=True)
 
         
         original1 = Image.open('MicrosoftTeams-image.png')
         kpi2_col.image(original1, width=40)
-       # kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="medium-font"> <b> Inviata a: </b></p></div>', unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="med"> <b> Request Claim team </b></p></div>', unsafe_allow_html=True)
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
@@ -172,7 +167,6 @@ if example == 'Email C':
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
-       # kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         original2 = Image.open('warning.PNG')
         kpi2_col.image(original2, width = 80)
 
@@ -182,17 +176,18 @@ if example == 'Email C':
 if example == 'Email D':
     
     st.write('\n\n\n')
-    #st.write("DATA RICEZIONE EMAIL: '2021-05-01'")
-    #st.write("Buongiorno,\n\nVi inoltro nuovamente mail, già inviatavi in data 13 aprile e della quale non ho ricevuto, come di consueto, la risposta automatica con l’indicazione del nr pratica assegnato. In attesa di vostro cortese riscontro, porgo cordiali saluti.\n\nFederica Rebolino\n\nmartedì 13 aprile 2021, 09:03 +0200 da Roberto e Federica <rebo.ricc@libero.it>:\n\nBuongiorno,\n\nVi riporto i dati per ottenere il rimborso di € 43,29 relativi alla fattura nr 502001905884 del 12/10/2020 scadenza il 2/11/2020, intestata a REBOLINO FEDERICA, nr cliente 10102553043, contratto nr. 00910012, in quanto pagata due volte. In attesa di vostro riscontro, porgo cordiali saluti.\n\nFederica Rebolino")
-    st.image('D.PNG')
-
     but1, but2, but3, but4, but5 = st.columns(5)
-    if (but3.button("RUN")):
+
+    if not (but3.button("RUN")):
+        st.image('D1.PNG')
+
+    else:
         gif_runner = st.image('https://media.giphy.com/media/Qw4X3Fsf0N1VqFFUiBi/giphy.gif')
         time.sleep(3)
         gif_runner.empty()
+
+        st.image('D.PNG')
         st.write('\n\n\n')
- 
         st.markdown('<div style="text-align:left"><p class="big-font">Results</p></div>', unsafe_allow_html=True)
         kpi1_col, kpi2_col = st.columns(2)
 
@@ -201,14 +196,12 @@ if example == 'Email D':
         kpi1_col.dataframe(df7)
         kpi1_col.markdown('<div style="text-align:left"><p class="medium-font"> <b> Informazioni personali estratte </b></p></div>', unsafe_allow_html=True)
         kpi1_col.dataframe(df8)
-
         kpi1_col.markdown('<div style="text-align:right"><p class="medium-font"> <b> Presenza ricicli: </b></p></div>', unsafe_allow_html=True)
         kpi1_col.markdown('<div style="text-align:right"><p class="med"> <b> SI </b></p></div>', unsafe_allow_html=True)
 
         
         original1 = Image.open('MicrosoftTeams-image.png')
         kpi2_col.image(original1, width=40)
-       # kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="medium-font"> <b> Inviata a: </b></p></div>', unsafe_allow_html=True)
         kpi2_col.markdown('<div style="text-align:center"><p class="med"> <b> Refund team </b></p></div>', unsafe_allow_html=True)
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
@@ -218,7 +211,6 @@ if example == 'Email D':
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
-        #kpi2_col.markdown("<p>&nbsp;</p>",  unsafe_allow_html=True)
         original2 = Image.open('warning.PNG')
         kpi2_col.image(original2, width = 70)
  
